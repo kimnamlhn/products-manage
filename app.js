ff63a200a8bc47344e68c8abc2c7eb6a09a7bade
kimnamlhn <kimnam.cpc@gmail.com>
2021-08-08 15:09:58 +0700

    .env

--- app.js
+++ app.js
@@ -7,22 +7,16 @@ const db  = require('./utils/db');
 // Storing configuration in the environment separate from code 
 require('dotenv').config()
 
-
-
 //connect database mongodb
 db.connect();
 
 //set static folder
 app.use(express.static(__dirname + '/resources'));
 
-
-
 //middlewares
 require('./middlewares/view.mdw')(app);
 require('./middlewares/routes.mdw')(app);
 require('./middlewares/error.mdw')(app);
-
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
